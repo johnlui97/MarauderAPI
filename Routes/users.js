493eda3827e3641a704e49c13a3df4f0e33df95b
John Lui <John Lui>
2021-01-02 12:02:58 -0500

    Starting to create backend functionality for onboarding/signing up a new user. Wrote new post route, that only prints a statement for now.

--- Routes/users.js
+++ Routes/users.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    console.log(`Attempting to get all the users`);
-    res.send("Acquired all users in database.")
-  });
+  console.log(`Attempting to get all the users`);
+  res.send("Acquired all users in database.");
+});
+
+router.post("/register", (req, res) => {
+  res.send("Registering new User into Marauder Backend.");
+});
 
 module.exports = router;
