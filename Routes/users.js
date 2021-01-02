f20703ff3750f52fa078735ed55ae4d5977d5e11
John Lui <John Lui>
2021-01-02 15:09:01 -0500

    Added Connection.js in order to reuse new conenction code to the database in the various routes.

--- Routes/users.js
+++ Routes/users.js
@@ -2,22 +2,18 @@ const { json } = require("express");
 const express = require("express");
 const router = express.Router();
 const mysql  = require("mysql");
-
-// Creating MySQL Connection
-var db = mysql.createConnection({
-  host:"marauder-db.clm4xkmydujh.us-east-2.rds.amazonaws.com",
-  user:"admin",
-  password:"January-1997-October",
-  database:"MarauderDB"
-});
+const db = require("../connection");
 
 router.get("/", (req, res) => {
   console.log(`Attempting to get all the users`);
-  var get_all_users_querry = "SELECT * FROM users";
-  let db_querry = db.query(get_all_users_querry, (err, results) => {
-    if(err) throw err;
-    console.log(results);
-    res.send(json(results));
+  var get_all_users_querry = `SELECT * FROM users`;
+  db.query(get_all_users_querry, (err, rows) => {
+    if (err) {
+      console.log(`Failed to query for users`, err);
+      return res.sendStatus(404);
+    }
+    console.log(`Completed fetching for the user`);
+    return res.json(rows);
   });
 });
 
---
+++ connection.js
@@ -0,0 +1,10 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host:"marauder-db.clm4xkmydujh.us-east-2.rds.amazonaws.com",
+    user:"admin",
+    password:"January-1997-October",
+    database:"MarauderDB"
+});
+
+module.exports = connection;
