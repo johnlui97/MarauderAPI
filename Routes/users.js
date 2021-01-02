f534645096b3a799caecab156f105642396ce34d
John Lui <John Lui>
2021-01-02 13:25:44 -0500

    Connecting Database to users route, and writing a test querry.

--- Routes/users.js
+++ Routes/users.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const mysql  = require("mysql");
+
+// Creating MySQL Connection
+var db = mysql.createConnection({
+  host:"marauder-db.clm4xkmydujh.us-east-2.rds.amazonaws.com",
+  user:"admin",
+  password:"January-1997-October",
+  database:"MarauderDB"
+});
 
 router.get("/", (req, res) => {
   console.log(`Attempting to get all the users`);
+  db.connect((err) => {
+    if(err) {
+        throw err;
+    }
+    console.log("Database Connected.");
+  });
   res.send("Acquired all users in database.");
 });
 
