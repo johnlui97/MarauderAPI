bdbb6290a4a963ac0bae3b6acdd5f1c39501dbf5
John Lui <John Lui>
2021-01-03 15:15:33 -0500

    Added login route which validates the user credentials, as well as adding a git ignore first ignoring node modules file.

--- Routes/users.js
+++ Routes/users.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const mysql  = require("mysql");
 const db = require("../connection");
 const { v4: uuidv4 } = require("uuid");
+const bcrypt = require("bcrypt")
 
 router.get("/", (req, res) => {
   console.log(`Attempting to get all the users`);
@@ -20,6 +21,9 @@ router.get("/", (req, res) => {
 
 router.post("/register", (req, res) => {
   const uniqueId = uuidv4();
+  const salt = bcrypt.genSaltSync(10);
+  const hashed_password = bcrypt.hashSync(req.body.password, salt);
+
   const user = {
     user_id: uniqueId,
     first_name: req.body.first_name,
@@ -27,7 +31,7 @@ router.post("/register", (req, res) => {
     email: req.body.email,
     age: req.body.age,
     gender: req.body.gender,
-    password: req.body.password,
+    password: hashed_password,
     image_link: req.body.image_link
   };
 
@@ -39,6 +43,29 @@ router.post("/register", (req, res) => {
       return res.sendStatus(500);
     }
     res.send("Successfully registered new User into Marauder Backend.");
+  });
+});
+
+router.post("/login", (req, res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password
+  };
+  const targetted_email = req.body.email;
+  const getUserByEmailQuerry = `SELECT * FROM users WHERE email = '${targetted_email}';`;
+
+  db.query(getUserByEmailQuerry, (err, rows) => {
+    if(err) {
+      return res.send(err);
+    }
+    queried_password = rows[0].password;
+    if(bcrypt.compareSync(req.body.password, queried_password)) {
+      console.log(true);
+      return res.send("Successfully Validated!");
+    } else {
+      console.log(false);
+      return res.send("Unsucessful Validation.");
+    }    
   });
 });
 
