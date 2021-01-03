6f0d61cae05a68a153cc54d2a95e371eb7492487
John Lui <John Lui>
2021-01-02 19:19:55 -0500

    Added users post route in order to add into databse from api.

--- Routes/users.js
+++ Routes/users.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 const mysql  = require("mysql");
 const db = require("../connection");
+const { v4: uuidv4 } = require("uuid");
 
 router.get("/", (req, res) => {
   console.log(`Attempting to get all the users`);
@@ -18,7 +19,26 @@ router.get("/", (req, res) => {
 });
 
 router.post("/register", (req, res) => {
-  res.send("Registering new User into Marauder Backend.");
+  const uniqueId = uuidv4();
+  const user = {
+    id: uniqueId,
+    first_name: 'John',
+    last_name: 'Doe',
+    email: 'doejoh@mcmaster.ca',
+    age: 23,
+    gender: 'MALE',
+    password: '12345abcde',
+    image_link: 'World Hello!'
+  };
+  var inser_user_querry = `INSERT INTO users SET ?`;
+
+  db.query(inser_user_querry, user, (err, rows) => {
+    if (err) {
+      console.log("Failed to register new user into Database, error: ", err);
+      return res.sendStatus(500);
+    }
+    res.send("Successfully registered new User into Marauder Backend.");
+  });
 });
 
 module.exports = router;
