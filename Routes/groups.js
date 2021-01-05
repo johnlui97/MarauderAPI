b6d2d89ea3905336ba4e6b509e1391e107e138c6
John Lui <John Lui>
2021-01-04 19:16:46 -0500

    Added starting boiler plate code for groups.js and for handling basic group outing creation.

--- Routes/groups.js
+++ Routes/groups.js
@@ -4,6 +4,16 @@ const router = express.Router();
 router.get("/", (req, res) => {
     console.log(`Attempting to get all Groups in Backend.`);
     res.send("Acquired all groups in database.");
-  });
+});
+
+router.post("/", (req, res) => {
+    console.log("Creating a new group event!");
+
+    // Adding logic to handle creating new group events
+    // 4 Maximum members, and will need mysql statements to create
+    // entries into multiple tables.
+
+    res.send("Creating new Group Outing");
+});
 
 module.exports = router;
