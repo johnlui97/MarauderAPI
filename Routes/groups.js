a59834352b29b834d74c39636b549c54bcbc294f
John Lui <John Lui>
2021-01-05 19:02:49 -0500

    Added routes into groups and outings. In order to begin facilitating matches and groups.

--- Routes/groups.js
+++ Routes/groups.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const { v4: uuidv4 } = require("uuid");
+const mysql  = require("mysql");
+const db = require("../connection");
+const { json } = require("body-parser");
 
 router.get("/", (req, res) => {
     console.log(`Attempting to get all Groups in Backend.`);
@@ -9,11 +13,30 @@ router.get("/", (req, res) => {
 router.post("/", (req, res) => {
     console.log("Creating a new group event!");
 
-    // Adding logic to handle creating new group events
-    // 4 Maximum members, and will need mysql statements to create
-    // entries into multiple tables.
+    const group_id  = uuidv4();
+    const user_id_1 = req.body.user_id_1;
+    const user_id_2 = req.body.user_id_2;
+    const user_id_3 = req.body.user_id_3;
+    const user_id_4 = req.body.user_id_4;
 
-    res.send("Creating new Group Outing");
+    const insertion_statement = `INSERT INTO groups(group_entry_id, group_id, user_id) VALUES ?;`;
+
+    if(user_id_1 && user_id_2 && user_id_3 && user_id_4) {
+        var user_array = [user_id_1, user_id_2, user_id_3, user_id_4];
+        var insertion_array = [];
+        for(i = 0; i < user_array.length; i++) {
+            var unique_entry_id = uuidv4();
+            insertion_array.push([unique_entry_id, group_id, user_array[i]]);
+        }
+        db.query(insertion_statement, [insertion_array], (err, result) => {
+            if (err) {
+                console.log("MarauderAPI - /groups POST failed to register new group, error: ", err);
+                return res.sendStatus(500);
+            }
+            console.log("MarauderAPI - /groups POST failed to register new group");
+            res.sendStatus(200);
+        });
+    }
 });
 
 module.exports = router;
--- Routes/outings.js
+++ Routes/outings.js
@@ -1,9 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const { v4: uuidv4 } = require("uuid");
+const mysql  = require("mysql");
+const db = require("../connection");
+const { json } = require("body-parser");
 
 router.get("/", (req, res) => {
     console.log(`Attempting to get all the outings.`);
     res.send("Acquired all outings in database.");
+});
+
+router.post("/", (req, res) => {
+    console.log("Generating new outings in outings table.");
+
+    const sample_group_id = uuidv4();
+    const match = null;
+    const venue_id = '35f1289c-aaa1-4c68-a56d-976e0387231a';
+    const timestamp = Date.now();
+
+    const outing = {
+      outing_id:venue_id,
+      group_id:sample_group_id,
+      match_id: match,
+      venue_id: venue_id,
+      time:timestamp
+    }
+
+    const insertion_statement = `INSERT INTO outings SET ?;`
+
+    db.query(insertion_statement, outing, (err, rows) => {
+      if(err) {
+        console.log("There was an error inserting into outigns table: ", err);
+        return res.sendStatus(500);
+      }
+      console.log("Succesfully entered outing into the table.");
+      res.sendStatus(200);
+    });
   });
 
 module.exports = router;
