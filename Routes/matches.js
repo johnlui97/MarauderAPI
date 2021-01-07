ebf93d5f573176c1b06f86d58485fc1c5984f145
John Lui <John Lui>
2021-01-07 17:04:17 -0500

    Trying to Debug aws server.

--- Routes/matches.js
+++ Routes/matches.js
@@ -4,6 +4,6 @@ const router = express.Router();
 router.get("/", (req, res) => {
     console.log(`Attempting to get all the matches.`);
     res.send("Acquired all matches in database.");
-  });
+});
 
 module.exports = router;
