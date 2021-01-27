20be13eb0dfd44ade9a5a4ab5358199dc2dc6995
John Lui <John Lui>
2021-01-27 13:15:26 -0500

    Uploading changes to solve 502 bad gateway.

--- app.js
+++ app.js
@@ -18,21 +18,21 @@ var marauder_db = mysql.createConnection({
     database:process.env.DATABASE
 });
 
-const user_routes = require("./Routes/users");
-const venues_routes = require("./Routes/venues");
-const outings_routes = require("./Routes/outings");
-const groups_routes = require("./Routes/groups");
-const matches_routes = require("./Routes/matches");
-const messages_routes = require("./Routes/messages");
-const friends_routes = require("./Routes/friends");
+// const user_routes = require("./Routes/users");
+// const venues_routes = require("./Routes/venues");
+// const outings_routes = require("./Routes/outings");
+// const groups_routes = require("./Routes/groups");
+// const matches_routes = require("./Routes/matches");
+// const messages_routes = require("./Routes/messages");
+// const friends_routes = require("./Routes/friends");
 
-app.use("/users", user_routes);
-app.use("/venues", venues_routes);
-app.use("/outings", outings_routes);
-app.use("/groups", groups_routes);
-app.use("/matches", matches_routes);
-app.use("/messages", messages_routes);
-app.use("/friends", friends_routes);
+// app.use("/users", user_routes);
+// app.use("/venues", venues_routes);
+// app.use("/outings", outings_routes);
+// app.use("/groups", groups_routes);
+// app.use("/matches", matches_routes);
+// app.use("/messages", messages_routes);
+// app.use("/friends", friends_routes);
 
 app.get('/', (req, res) => {
     console.log("Trying to establish Connection to databse.");
